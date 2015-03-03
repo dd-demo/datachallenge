1dc21824c29fe7de8847ad0f709bc8839f8bb8af
Duong DANG <ngoc-duong.dang@amadeus.com>
2015-03-03 21:00:47 +0100

    Add the webservice (bonus).

---
+++ websrv/flask_server.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+
+import pandas as pd
+import os
+from flask import Flask, jsonify
+import pickle
+
+sorted_airports = None
+app = Flask(__name__)
+
+CURR_DIR = os.path.dirname(__file__)
+@app.route('/top_airports/<n>')
+def topn(n):
+    try:
+        n = int(n)
+    except:
+        return jsonify({"status": "failure", "message": "Invalid numerical value"})
+    data = []
+    rank = 1
+    for index, row in sorted_airports[:n].iterrows():
+        data.append({ "rank" : rank, "code": index, "pax": row[0]})
+        rank +=1
+    return jsonify({"status": "success", "airports": data})
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return ''''Page not found.<br>
+URL to top N airports:<br><br>
+/top_airports/N'''
+
+def load_from_pickle(fn):
+    try:
+        return pickle.load(open(fn))
+    except:
+        print "Pickle does not exist or corrupted, reloading..."
+        CHUNKSIZE=100000
+        pieces = [pd.DataFrame(chunk)
+                  for chunk in pd.read_csv(os.path.join(CURR_DIR, "..", "bookings.csv"),
+                                           chunksize = CHUNKSIZE,
+                                           delimiter = r" *\^",
+                                           usecols=["arr_port", "pax"])]
+        df = pd.concat(pieces)
+        df = df.dropna()
+        res = df.groupby("arr_port").sum().sort('pax', ascending=False)
+        pickle.dump(res, open(fn, "wb"))
+        return res
+
+if __name__ == '__main__':
+    pickle_file = os.path.join(CURR_DIR, "airports.pickle")
+    sorted_airports = load_from_pickle(pickle_file)
+    app.run(host = "0.0.0.0")
